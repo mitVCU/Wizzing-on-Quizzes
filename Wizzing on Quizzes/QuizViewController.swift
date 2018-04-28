608e68503ef5480854c7df04384f22ee179c0470
moriah-hammond <moriah.hammond@snagajob.com>
2018-04-27 23:09:18 -0400

    here

--- Wizzing on Quizzes/QuizViewController.swift
+++ Wizzing on Quizzes/QuizViewController.swift
@@ -9,37 +9,46 @@
 import Foundation
 import UIKit
 
-class QuizViewController {
+class QuizViewController: UIViewController {
     
     //Mark IB-Outlets
-    @IBOutlet weak var aButton: UIButton!
-    @IBOutlet weak var bButton: UIButton!
-    @IBOutlet weak var cButton: UIButton!
-    @IBOutlet weak var dButton: UIButton!
-    @IBOutlet weak var person1: UIImageView!
-    @IBOutlet weak var person2: UIImageView!
-    @IBOutlet weak var person3: UIImageView!
-    @IBOutlet weak var person4: UIImageView!
-    @IBOutlet weak var p1Answer: UILabel!
-    @IBOutlet weak var p2Answer: UILabel!
-    @IBOutlet weak var p3Answer: UILabel!
-    @IBOutlet weak var p4Answer: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var aAnswer: UIButton!
+    @IBOutlet weak var bAnswer: UIButton!
+    @IBOutlet weak var cAnswer: UIButton!
+    @IBOutlet weak var dAnswer: UIButton!
+
+    @IBOutlet weak var question: UILabel!
+    @IBOutlet weak var questionCount: UILabel!
     @IBOutlet weak var p1Score: UILabel!
     @IBOutlet weak var p2Score: UILabel!
     @IBOutlet weak var p3Score: UILabel!
     @IBOutlet weak var p4Score: UILabel!
-    @IBOutlet weak var questionNumber: UILabel!
-    @IBOutlet weak var question: UILabel!
+    @IBOutlet weak var p4: UIImageView!
+    @IBOutlet weak var p3: UIImageView!
+    @IBOutlet weak var p2: UIImageView!
+    @IBOutlet weak var p1: UIImageView!
     
-    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var p4Answer: UILabel!
+    @IBOutlet weak var p3Answer: UILabel!
+    @IBOutlet weak var p2Answer: UILabel!
+    @IBOutlet weak var p1Answer: UILabel!
     
     @IBAction func aTapped(_ sender: UIButton) {
     }
+    
     @IBAction func bTapped(_ sender: UIButton) {
+        
+    }
+    @IBAction func cAnswer(_ sender: UIButton) {
+        
+    }
+    @IBAction func dAnswer(_ sender: UIButton) {
+        
     }
     
-    @IBAction func cTapped(_ sender: Any) {
-    }
-    @IBAction func dTapped(_ sender: UIButton) {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
 }

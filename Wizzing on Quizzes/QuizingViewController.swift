b7a7dea33bb6921f311ada73b3713063d7583d41
Mit Amin <aminm2@vcu.edu>
2018-05-04 19:50:53 -0400

    working communication between devices

---
+++ Wizzing on Quizzes/QuizingViewController.swift
@@ -0,0 +1,22 @@
+//
+//  QuizingViewController.swift
+//  Wizzing on Quizzes
+//
+//  Created by Mit Amin on 4/27/18.
+//  Copyright © 2018 Mit Amin. All rights reserved.
+//
+
+import UIKit
+
+class QuizingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+}
